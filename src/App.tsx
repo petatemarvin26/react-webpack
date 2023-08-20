905c307eb5ba4f3374c02b292c8effeb0590ac59
Marvin Petate <46154422+petatemarvin26@users.noreply.github.com>
2023-08-20 10:37:43 +0800

    relase/1.2.4 (#108)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import styles from './App.module.css';
 
 const App: React.FC = () => {
-  return <div className={styles.main}>HELLO WORLD! {process.env.VERSION}</div>;
+  return <div className={styles['main']}>HELLO WORLD!</div>;
 };
 
 export default App;
