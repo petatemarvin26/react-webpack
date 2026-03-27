6d479fdc796fd73920c6e380d3152f34e5f9fe79
Marvin Petate <46154422+petatemarvin26@users.noreply.github.com>
2026-03-27 15:11:03 +0800

    release/1.6.1 (#134)

--- config/webpack/common/constants.js
+++ config/webpack/common/constants.js
@@ -1,9 +1,12 @@
+const dotenv = require('dotenv');
+const vars = dotenv.config({path: '.env'}).parsed;
+
 const path = require('path');
 
 const PORT = process.env.PORT || 3000;
 const HOST = process.env.HOST || 'localhost';
 const VERSION = process.env.npm_package_version;
-const PUBLIC_URL = process.env.PUBLIC_URL || `http://${HOST}:${PORT}`;
+const PUBLIC_URL = vars.PUBLIC_URL || `http://${HOST}:${PORT}`;
 
 const ROOT_DIR = path.resolve(__dirname, '../../..');
 
