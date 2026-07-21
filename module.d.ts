83f73cf461e422dade0b44e2ce6fea3e2ff8e300
Marvin <petatemarvin26@gmail.com>
2026-07-21 16:46:12 +0800

    release/2.0.2

--- module.d.ts
+++ module.d.ts
@@ -2,11 +2,13 @@ declare module '*.json';
 
 declare module '*.module.css';
 declare module '*.css';
+declare module '*.scss';
 
 declare module '*.gif';
 declare module '*.png';
 declare module '*.jpeg';
 declare module '*.jpg';
+declare module '*.webp';
 
 declare module '*.svg' {
   const ReactComponent: React.ElementType<React.SVGAttributes<SVGElement>>;
